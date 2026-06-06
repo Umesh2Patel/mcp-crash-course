f3ed5de1abfaefcd3903eebd1ceb27b024d49fe0
umacpro <umeshp203@gmail.com>
2026-06-05 18:14:47 -0700

    commit 3

--- main.py
+++ main.py
@@ -12,3 +12,24 @@ from mcp.client.stdio import stdio_client
 load_dotenv()
 
 llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/umacpro/mcp-crash-course/server/math_server.py"],
+)
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+
+
+            agent = create_react_agent(llm,tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+
+if __name__ == "__main__":
+    asyncio.run(main())
